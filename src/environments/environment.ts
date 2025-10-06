707450681334ccdff2d6ac72196a6e8d97ad5b11
mnadeem17 <muhammad.nadeem@lits.services>
2025-10-06 19:47:13 +0500

    Local and UAT configurations
    Local and UAT configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
+
+//Local
 export const environment = {
  production: false,
   apiUrl: 'https://localhost:7188/api',
  
 };
+
+//UAT
+// export const environment = {
+//  production: false,
+//   apiUrl: 'http://192.168.7.201:98/api',
+ 
+// };
