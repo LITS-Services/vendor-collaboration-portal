9be6f5f04efeca47c37868716199ac9b193c2cd7
sahalsultan <sahal.sultan@lits.services>
2025-10-23 12:53:56 +0500

    get purchase order list by vendor
    get purchase order list by vendor

---
+++ src/app/shared/services/purchase-order.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PurchaseOrderService {
+  private baseUrl = `${environment.apiUrl}/PurchaseOrder`;
+  constructor(private http: HttpClient) { }
+
+  getPurchaseOrdersByVendor(vendorUserId: string): Observable<any> {
+    const params = new HttpParams().set('vendorUserId', vendorUserId);
+    return this.http.get<any>(`${this.baseUrl}/get-purchase-orders-by-vendor`, { params });
+  }
+}
